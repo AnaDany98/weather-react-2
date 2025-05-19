78f33088ff4d7cba74ef4ec886e619906136b66a
AnaDany98 <105895422+AnaDany98@users.noreply.github.com>
2025-05-19 14:16:34 -0400

    Added the LinkedIn link

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import "./App.css";
 import Weather from "./Weather";
 
 export default function App(){
-  return <div className="App">
-    <div className="container">
-      <h1>Weather App</h1>
-      <Weather />
-      <footer>
-        This project was coded by <a href="#" target="_blank">Ana Daniela Garcia Lara</a> and is
-        <a href="https://github.com/AnaDany98/weather-react-2/tree/main" target="_blank"> open-sourced on GitHub</a>
-      </footer>      
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/ana-daniela-garc%C3%ADa-lara-001336216/"
+            target="_blank"
+          >
+            Ana Daniela Garcia Lara
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/AnaDany98/weather-react-2/tree/main"
+            target="_blank"
+          >
+            {" "}
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
-
-    
-    </div>
+  );
 }
 
 
