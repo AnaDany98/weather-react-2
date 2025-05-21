f563e4ef6f1aa3abb1df94ff4241f745ba6e03de
AnaDany98 <105895422+AnaDany98@users.noreply.github.com>
2025-05-21 12:48:27 -0400

    Changed the Icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -15,11 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-3">
         <div className="col-6">
           <div className="d-flex align-items-center">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              width={52}
-            />
+            <WeatherIcon code={props.data.icon} size={52} />
             <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
